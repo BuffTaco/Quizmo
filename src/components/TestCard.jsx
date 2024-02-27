ce7a334610e85f3783bae56e08ce80221b6cdf30
BuffTaco <jiayue.quan@gmail.com>
2024-02-26 16:08:22 -0800

    card flip

--- src/components/TestCard.jsx
+++ src/components/TestCard.jsx
@@ -1,9 +1,14 @@
-import '../App.css'
+import '../Card.css'
+import { useState } from 'react'
+
 function TestCard() {
+    const [activeness, setActiveness] = useState('not')
     return (
         <div className="cardContainer">
-            <div className="card">
-                <div className="front">Front</div>
+            <div className="card" onClick={() => {
+                setActiveness(activeness == 'active' ? 'not' : 'active')
+                }}  id={activeness}>
+                <div  className="front">Front</div>
                 <div className="back">Back</div>
             </div>
         </div>
