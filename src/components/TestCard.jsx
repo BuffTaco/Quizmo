3fce1d5b3767d48eca8b7d1081659c5fd4d2a2d9
BuffTaco <jiayue.quan@gmail.com>
2024-03-11 15:16:09 -0700

    move between cards

--- src/components/TestCard.jsx
+++ src/components/TestCard.jsx
@@ -1,17 +1,85 @@
 import '../Card.css'
 import { useState } from 'react'
 
+const temp = [
+    {
+        "front": "1",
+        "back": "2"
+    },
+    {
+        "front": "3",
+        "back": "4"
+    },
+    {
+        "front": "5",
+        "back": "6"
+    },
+    {
+        "front": "7",
+        "back": "8"
+    },
+    {
+        "front": "9",
+        "back": "0"
+    },
+]
+let index = 0;
 function TestCard() {
+    
+    
+    
+    
     const [activeness, setActiveness] = useState('not')
+    const [front, setFront] = useState(temp[index].front)
+    const [back, setBack] = useState(temp[index].back)
+    
     return (
+        <>
         <div className="cardContainer">
             <div className="card" onClick={() => {
                 setActiveness(activeness == 'active' ? 'not' : 'active')
                 }}  id={activeness}>
-                <div  className="front">Front</div>
-                <div className="back">Back</div>
+                <div  className="front">{front}</div>
+                <div className="back">{back}</div>
             </div>
         </div>
+        <div className="travelButtons">
+            <br/>
+            <button onClick={() => {
+                index--;
+                if (index >= 0)
+                {
+                    
+                    setFront(temp[index].front)
+                    setBack(temp[index].back)
+                }
+                else {
+                    index = temp.length-1   ;
+                    setFront(temp[index].front)
+                    setBack(temp[index].back)
+                }
+                
+                
+            }}>Back</button>
+            <button onClick={() => {
+                index++;
+                if (index < temp.length)
+                {
+                    
+                    setFront(temp[index].front)
+                    setBack(temp[index].back)
+                }
+                else {
+                    index = 0;
+                    setFront(temp[index].front)
+                    setBack(temp[index].back)
+                }
+                
+                
+            }}>Next</button>
+        </div>
+        </>
+
     )
 }
 export default TestCard
